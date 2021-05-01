1e691575ba360e25f82cddf0e8e6a4e7534ca198
RezzedUp <RezzedUp@users.noreply.github.com>
2021-05-01 13:17:13 -0500

    Initial commit.

---
+++ src/main/java/com/rezzedup/valuable/DefaultValue.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright © 2021, RezzedUp <https://github.com/RezzedUp/Valuable>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.rezzedup.valuable;
+
+public interface DefaultValue<S, T> extends DefaultValueGetter<S, T>, DefaultValueSetter<S, T>,  Value<S, T> {}
---
+++ src/main/java/com/rezzedup/valuable/ValueGetter.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2021, RezzedUp <https://github.com/RezzedUp/Valuable>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.rezzedup.valuable;
+
+import java.util.Optional;
+
+@FunctionalInterface
+public interface ValueGetter<S, T>
+{
+    Optional<T> get(S storage);
+}
